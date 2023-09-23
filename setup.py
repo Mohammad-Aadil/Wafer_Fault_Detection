a9665cfa612d8fe56859a6631a0669d93fce8e57
Mohammad-Aadil <shaikhaadil8855@gmail.com>
2023-09-23 19:13:12 +0530

    changes

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="0.0.1",
-    author="iNeuron",
-    author_email="cloud@ineuron.ai",
+    author="Mohammad_Aadil",
+    author_email="shaikhaadil8855@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
